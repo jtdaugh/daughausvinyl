6858747f2a59fcb97e00b43d427c65235d4e4d1c
Jesse Daugherty <jesse.daugherty@albert.com>
2023-12-31 10:58:05 -0800

    more responsive but not perf

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -73,6 +73,7 @@ export const Home = () => {
   const [searchVisible, setSearchVisible] = useState(false);
   const collectionRef = useRef<HTMLDivElement>(null);
   const wishlistRef = useRef<HTMLDivElement>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchString(event.target.value);
@@ -126,11 +127,11 @@ export const Home = () => {
 
   return (
     <div className="flex-1 w-full p-6 pt-2 max-w-[1600px] min-h-screen pt-16">
-      <div className="fixed py-4 top-0 left-0 right-0 z-10 border-b border-gray-300/30 bg-[#702707]">
+      <div className="fixed py-4 top-0 left-0 right-0 z-10 border-b border-gray-300/30 bg-[#702707] hidden md:block">
         <div className="flex items-center justify-center h-16">
-        <button
+          <button
             onClick={scrollToWishlist}
-            className=" font-serif  px-4 py-2 mx-2 text-white hover:text-gray-300"
+            className="font-serif px-4 py-2 mx-2 text-white hover:text-gray-300"
           >
             Wishlist
           </button>
@@ -140,36 +141,88 @@ export const Home = () => {
           >
             Collection
           </button>
-          <div className="text-4xl my-3 text-[#eee] font-rubik px-8">DaugHaus Vinyl
-            
-          </div>
-          
+          <div className="text-4xl my-3 text-[#eee] font-rubik px-8">DaugHaus Vinyl</div>
           <button
             onClick={toggleSearch}
-            className=" font-serif  px-4 py-2 mx-2 text-white hover:text-gray-300"
+            className="font-serif px-4 py-2 mx-2 text-white hover:text-gray-300"
           >
             Search
           </button>
-          <span
-            className=" font-serif  px-4 py-2 mx-2 text-[#aaa] hover:text-gray-300"
-          >
-            Est. 2023
-          </span>
-          
+          <span className="font-serif px-4 py-2 mx-2 text-[#aaa] hover:text-gray-300">Est. 2023</span>
         </div>
         {searchVisible && (
-        <div className="w-full px-6">
-          <input
-          type="text"
-          value={searchString}
-          onChange={handleSearchChange}
-          placeholder="Search by artist or release title"
-          className="w-full mt-4 p-2 border border-gray-300 rounded-md bg-gray-200"
-        />
-        </div>
+          <div className="w-full px-6">
+            <input
+              type="text"
+              value={searchString}
+              onChange={handleSearchChange}
+              placeholder="Search by artist or release title"
+              className="w-full mt-4 p-2 border border-gray-300 rounded-md bg-gray-200"
+            />
+          </div>
         )}
       </div>
-      
+      <div className="fixed py-3 top-0 left-0 right-0 z-10 border-b border-gray-300/30 bg-[#702707] visible md:hidden">
+        <div className="flex flex-row items-center justify-between w-full">
+            <div>
+              <div className="text-2xl my-2 text-[#eee] font-rubik px-8">DaugHaus Vinyl</div>
+            </div>
+            <div>
+              <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="font-serif px-4 py-2 mx-2 text-white hover:text-gray-300"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        {isMenuOpen && (
+          <div className="md:hidden">
+            <button
+              onClick={scrollToWishlist}
+              className="font-serif block px-4 py-2 mx-2 text-white hover:text-gray-300"
+            >
+              Wishlist
+            </button>
+            <button
+              onClick={scrollToCollection}
+              className="font-serif block px-4 py-2 mx-2 text-white hover:text-gray-300"
+            >
+              Collection
+            </button>
+            <button
+              onClick={toggleSearch}
+              className="font-serif block px-4 py-2 mx-2 text-white hover:text-gray-300"
+            >
+              Search
+            </button>
+            {searchVisible && (
+            <div className="w-full px-6">
+              <input
+                type="text"
+                value={searchString}
+                onChange={handleSearchChange}
+                placeholder="Search by artist or release title"
+                className="w-full mt-4 p-2 border border-gray-300 rounded-md bg-gray-200"
+              />
+            </div>
+          )}
+          </div>
+        )}
+      </div>
      
       <h1 ref={wishlistRef} className="text-3xl mb-3 text-[#eee] font-serif pt-24">
         Wishlist
